7c05db85f56a6ff9e9184fbc82f92a7f6ed781ca
ggiou <majiou4420@gmail.com>
2025-02-11 17:11:43 +0900

    스켈레톤 기본 패키지

---
+++ src/test/java/com/clone/rottentomato/RottentomatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.rottentomato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RottentomatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
