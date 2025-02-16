478edae56de4d13633c77f633b6f1476720815cf
ggiou <majiou4420@gmail.com>
2025-02-17 08:53:09 +0900

    [movie] 영화 기본패키지 추가

---
+++ src/main/java/com/clone/rottentomato/domain/movie/component/entity/Movie.java
@@ -0,0 +1,18 @@
+package com.clone.rottentomato.domain.movie.component.entity;
+
+import com.clone.rottentomato.common.component.entity.TimeStamped;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Movie extends TimeStamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long movieId;
+}
