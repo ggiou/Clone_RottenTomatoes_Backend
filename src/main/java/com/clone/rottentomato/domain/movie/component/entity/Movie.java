1feec0592a1ba8e893fc669a7bf8b3d81a0f1c23
ggiou <majiou4420@gmail.com>
2025-02-18 00:37:21 +0900

    [movie] 영화 예고편 엔티티 추가 및 엔티티 default 값 수정

--- src/main/java/com/clone/rottentomato/domain/movie/component/entity/Movie.java
+++ src/main/java/com/clone/rottentomato/domain/movie/component/entity/Movie.java
@@ -5,7 +5,6 @@ import com.clone.rottentomato.common.fomatter.DefaultDateFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 
 import java.math.BigDecimal;
@@ -19,13 +18,10 @@ public class Movie extends TimeStamped {
     @Column(nullable = false)
     @Size(max = 100, message = "영화 제목은 최대 100자까지 입력가능합니다.")
     private String name;    // 영화명
-    @Column(columnDefinition = "decimal(2, 1) default 0.0 max 5.0")
-    private BigDecimal rating;  // 영화 평점 (리뷰 별점 기준)
-    @ColumnDefault("")  // FIXME 데이터 없을 경우 기본 이미지 URL 지정 후 세팅
-    private String posterUrl;   // 영화 포스터 url
+    @Column(columnDefinition = "decimal(2, 1) default 0.0 check (rating >= 0.0 and rating <= 5.0)")
+    private BigDecimal rating;  // 영화 평점 (리뷰 별점 기준 -> 0.0 ~ 5.0)
+    @Column(columnDefinition = "varchar(255) default ''")
+    private String posterUrl;   // 영화 포스터 url       // FIXME 데이터 없을 경우 기본 이미지 URL 지정 후 세팅
     @DefaultDateFormat
     private LocalDateTime releaseDate;  // 개봉일
-
-    @OneToOne(mappedBy = "movieDetail", fetch = FetchType.LAZY)
-    private Movie movieDetail;  // 영화 상세 정보 (lazy 방식으로, 필요할때 따로 쿼리를 통해 조회)
 }
