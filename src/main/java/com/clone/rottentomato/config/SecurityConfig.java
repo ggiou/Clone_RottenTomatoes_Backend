86a140c06cf08eaa249c49eac17e30fe36d7c693
Kuno1734 <dlsgus59@gmail.com>
2025-02-18 22:53:34 +0900

    feat : 시큐리티 설정 변경   - h2-console 접속시 구글 인증페이지로 가는 현상 수정.

--- src/main/java/com/clone/rottentomato/config/SecurityConfig.java
+++ src/main/java/com/clone/rottentomato/config/SecurityConfig.java
@@ -8,21 +8,26 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private final GoogleMemberService googleMemberService;
-
-    public SecurityConfig(GoogleMemberService googleMemberService) {
-        this.googleMemberService = googleMemberService;
-    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/h2-console/**") // H2 Console은 CSRF 제외
+                )
+                .headers(headers -> headers
+                        .frameOptions(frame -> frame.disable()) // H2 Console 프레임 허용
+                )
+
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo
                                 .userService(googleMemberService)
