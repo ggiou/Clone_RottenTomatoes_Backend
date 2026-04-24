c7054caa39a388084f6a75f4e0d33411e5b96d52
ggiou <110371892+ggiou@users.noreply.github.com>
2026-04-24 09:10:17 +0900

    mail config 개인정보 삭제

--- src/main/java/com/clone/rottentomato/config/MailConfig.java
+++ src/main/java/com/clone/rottentomato/config/MailConfig.java
@@ -9,14 +9,20 @@ import java.util.Properties;
 
 @Configuration
 public class MailConfig {
+    
+    @Value("${mail.naver.username}")
+    private String username;
+
+    @Value("${mail.naver.password}")
+    private String password;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.naver.com");
         mailSender.setPort(587);
-        mailSender.setUsername("REMOVED_SECRET_EMAIL");
-        mailSender.setPassword("REMOVED_SECRET");
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
